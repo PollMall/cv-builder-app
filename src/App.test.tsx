12187c05c571f2b4e62ca315c6b9883859fc88f1
Paul Popa <paul.popa@moduscreate.com>
2021-07-12 19:55:47 +0300

    fixed tests for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App tests', () => {
-  test('renders learn react link', () => {
+  it('should render', () => {
     render(<App />);
-    expect(screen.getByText(/landing page/i)).toBeInTheDocument();
   });
 });
