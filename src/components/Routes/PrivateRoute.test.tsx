d0c35fde6eee3b57d752a2ef0f81a3591168fb96
Paul Popa <paul.popa@moduscreate.com>
2021-07-16 23:12:53 +0300

    fixed tests for private routes

--- src/components/Routes/PrivateRoute.test.tsx
+++ src/components/Routes/PrivateRoute.test.tsx
@@ -3,27 +3,28 @@ import { MemoryRouter as Router, Switch } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import PrivateRoute from './PrivateRoute';
 import Mock from './__mocks__/Mock';
-import { UserProvider } from '../../context/UserContext';
+import { AuthContext } from '../../context/AuthContext';
+import firebase from 'firebase/app';
 
-const renderApp = (isAuthenticated: boolean) =>
+const renderApp = (user: firebase.auth.UserCredential | undefined) =>
   render(
-    <UserProvider initialValue={{ isAuthenticated }}>
+    <AuthContext.Provider value={{ state: { loading: false, user }, dispatch: jest.fn() }}>
       <Router initialEntries={['/path']}>
         <Switch>
           <PrivateRoute component={Mock} path="/path" to="/redirect" />
         </Switch>
       </Router>
-    </UserProvider>,
+    </AuthContext.Provider>,
   );
 
 describe('PrivateRoute tests', () => {
   it('should render component', () => {
-    renderApp(true);
+    renderApp({} as firebase.auth.UserCredential);
     expect(screen.getByText(/mock/i)).toBeInTheDocument();
   });
 
   it('should redirect', () => {
-    renderApp(false);
+    renderApp(undefined);
     expect(screen.queryByText(/mock/i)).not.toBeInTheDocument();
   });
 });
