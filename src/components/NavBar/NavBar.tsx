246ae14f8ea4d798457cf5c0266095594b7feba8
Paul Popa <paul.popa@moduscreate.com>
2021-08-05 22:44:56 +0300

    click on logo redirects to home page

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { AppBar, Box } from '@material-ui/core';
+import { useHistory } from 'react-router';
+import { AppBar, Box, IconButton } from '@material-ui/core';
 import useStyles from './styles';
 import { LogoSvg } from '../../images';
 import Dropdown from './Dropdown';
 
 const NavBar = () => {
   const classes = useStyles();
+  const { push } = useHistory();
 
   return (
     <AppBar elevation={1} className={classes.root} position="fixed">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <LogoSvg className={classes.logo} />
+        <IconButton size="small" onClick={() => push('/')}>
+          <LogoSvg className={classes.logo} />
+        </IconButton>
         <Dropdown />
       </Box>
     </AppBar>
