c8e5600997169c93e771570b90312ab70be06074
Paul Popa <paul.popa@moduscreate.com>
2021-08-23 03:19:31 +0300

    changed apollo url

--- src/apolloConfig.ts
+++ src/apolloConfig.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://cv-builder-server-1.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
