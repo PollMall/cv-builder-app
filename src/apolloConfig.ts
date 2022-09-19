a29b35e57da6d6b1acc57f7599ff98614a932bd2
Paul Popa <paul.popa@moduscreate.com>
2022-09-19 18:57:55 +0300

    fix .env vars and make app work

--- src/apolloConfig.ts
+++ src/apolloConfig.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+console.log(process.env.REACT_APP_API_URL);
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: process.env.REACT_APP_API_URL,
   cache: new InMemoryCache(),
 });
 
